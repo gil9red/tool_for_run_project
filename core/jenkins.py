1787b49303bbbee7f7e24e59f18697304b76d2ab
gil9red <ilya.petrash@inbox.ru>
2026-02-16 13:31:34 +0500

    Исправление ошибки. Переход дженкинса на https

--- core/jenkins.py
+++ core/jenkins.py
@@ -5,6 +5,7 @@ __author__ = "ipetrash"
 
 
 from datetime import datetime
+from typing import Any
 
 import requests
 
@@ -15,23 +16,31 @@ class JenkinsJobCheckException(GoException):
     pass
 
 
-def do_check_jenkins_job(url: str, version: str):
-    url = url.format(version=version)
+# NOTE: InsecureRequestWarning: Unverified HTTPS request is being made to host 'jenkins-apd.compassplus.com'. Adding certificate verification is strongly advised. See: https://urllib3.readthedocs.io/en/1.26.x/advanced-usage.html#ssl-warnings
+#       Еще нужно в запросы добавлять verify=False
+#       Так проще, чем таскать с собой сертификат, или обязывать указывать его
+requests.packages.urllib3.disable_warnings(
+    requests.packages.urllib3.exceptions.InsecureRequestWarning
+)
 
-    rs = requests.get(url)
+
+def do_check_jenkins_job(url: str, version: str):
+    url: str = url.format(version=version)
+
+    rs = requests.get(url, verify=False)
     if rs.status_code == 404:
         print(f"[!] Сборки для версии {version} нет.")
         return
 
     rs.raise_for_status()
 
-    data = rs.json()
+    data: dict[str, Any] = rs.json()
 
     # "0:39:56.476184" -> "0:39:56"
     duration = datetime.now() - datetime.fromtimestamp(data["timestamp"] / 1000)
     duration_str = str(duration).split(".")[0]
 
-    result = data["result"]
+    result: str = data["result"]
     if not result:
         raise JenkinsJobCheckException(
             f"Сборка еще в процессе, прошло {duration_str}.\nCсылка: {data['url']}"
@@ -42,3 +51,13 @@ def do_check_jenkins_job(url: str, version: str):
             f"Сборка поломанная, обновление прервано. "
             f"С последнего запуска прошло {duration_str}.\nCсылка: {data['url']}"
         )
+
+
+if __name__ == "__main__":
+    from settings import get_project, run_settings_preprocess
+
+    run_settings_preprocess()
+
+    settings = get_project("optt")
+    jenkins_url: str = settings.get("jenkins_url")
+    do_check_jenkins_job(jenkins_url, "trunk")
